d9afd72f0640c8af7d1055ac0c457312ab13d0ae
bkancogl <44644273+bkancogl@users.noreply.github.com>
2018-12-05 22:04:50 -0500

    Add files via upload

--- client.js
+++ client.js
@@ -1,18 +1,15 @@
 var usedNums = new Array(79);
 var calledNums = [];
-var color = "rgba(10,255,10,.75)";
+var numCalled = []
+var color = "rgba(10, 255, 10, .75)";
 var socket = io();
-var j = -1;
 
-socket.on("updateUsers", function(usernames, money){
+socket.on("updateUsers", function(usernames){
    $("#listOfUsers").html("");
    for(i in usernames){
-      $("#listOfUsers").append(usernames[i]+" " + money + "<br>");
+      $("#listOfUsers").append(usernames[i]+"<br>");
    }
 });
-
-
-
 function initAll() {
    $("#loginForm").submit(function(event){
       //encrypt password
@@ -26,6 +23,8 @@ function initAll() {
                //$("#numberGenerator").show();
                $("#playButton").show();
                $("#bingo").show();
+               $("#postageStamp").show();
+               $("#fourCorners").show();
             }
             else{
                $("#userpass").show();
@@ -59,7 +58,9 @@ function initAll() {
                //$("#numberGenerator").show();
                $("#playButton").show();
                $("#bingo").show();
-               socket.emit("addUser", $("#newUsername").val(), hash($("#newPassword").val()), 0, 0, 900);
+               $("#postageStamp").show();
+               $("#fourCorners").show();
+               socket.emit("addUser", $("#newUsername").val(), $("#newPassword").val());
             }
             else{
                $("#userexists").show();
@@ -73,53 +74,123 @@ function initAll() {
       } 
       event.preventDefault();
    });
-   //make the server generate the cards
 
    $("#getCards").click(function(){
-      socket.emit("getCards", Number($("#selection option:selected").val()), function(enoughMoney){
-         if(enoughMoney==true){
-            $("#mytable").html("");
-            for(var i = 1; i <= $("#selection option:selected").val(); i ++){
-               makeCard(i);
-               //fix
-               anotherCard();
-            }
-         }
-         else{
-            console.log("Not enough money for that amount of cards");
-            $("#keepplaying").show();
-         }
-      })
-      
+      console.log($("#selection option:selected").val());
+      $("#mytable").html("");
+ //   pageBreaks = 'on';
+      for(var i = 1; i <= $("#selection option:selected").val(); i ++){
+         console.log("Creating card number " + i);
+         makeCard(i);
+         anotherCard();
+      }
    });
-   //make the server generate the random number
+   
    $(getRandNumDisplayed);
    $(clickHandler);
    
    $("#bingo").click(function(){
-      console.log("So you think you have bingo?");
       var count = 0;
       for(var i = 0; i < calledNums.length; i ++){
          if(usedNums[calledNums[i]]){
             count++;
          }
+         
       }
       if(count == 24){
          console.log("We have a winner, " + username + "won the game");
          $("#win").show();
-      }
-         
+      }      
       else{
          console.log("You don't have a bingo yet... keep playing");
          $("#keepplaying").show();
-      } 
+      }
    })
+
+   $("#postageStamp").click(function(){
+      for(var i = 0; i < calledNums.length; i++){
+         if(usedNums[calledNums[i]]){
+            if(($("#square0").css("background-color", color)) && ($("#square1").css("background-color",color)) && ($("#square5").css("background-color",color)) && ($("#square6").css("background-color", color))){
+               $("#postageStampWin").show();
+               console.log(document.getElementById("#square0"));
+            }
+            else if(($("#square3").css("background-color", color)) && ($("#square4").css("background-color",color)) && ($("#square8").css("background-color",color)) && ($("#square9").css("background-color", color))){
+               $("#postageStampWin").show();
+            }
+            else if(($("#square14").css("background-color", color)) && ($("#square15").css("background-color",color)) && ($("#square19").css("background-color",color)) && ($("#square20").css("background-color", color))){
+               $("#postageStampWin").show();
+            }
+            else if(($("#square17").css("background-color", color)) && ($("#square18").css("background-color",color)) && ($("#square22").css("background-color",color)) && ($("#square23").css("background-color", color))){
+               $("#postageStampWin").show();
+            }
+            else{
+               $("#notFinishedPostageStamp").show();
+            }      
+         }
+      }
+   })
+   $("#fourCorners").click(function(){
+      var count = 0;
+      console.log(typeof(String(document.getElementById("square0"))));
+      
+      for(var i = 0; i < calledNums.length; i++){
+         //console.log("type of calledNums: "+typeof(calledNums[i]));
+         console.log("type of doc: "+typeof((document.getElementById("square0"))));
+         console.log("called num: "+calledNums[i]);
+         //console.log("doc num: "+String(document.getElementById("square0").val()));
+         if(usedNums[calledNums[i]]){
+            if(calledNums[i] == document.getElementById("square0").innerHTML){
+               console.log("in");
+               count++;
+               //console.log("A = " + ($("square0").css("background-color")));
+               //console.log("B = " + color);
+            }
+            else if(calledNums[i] == document.getElementById("square19").innerHTML){
+               console.log("in");
+               count++;
+            }
+            else if(calledNums[i] == document.getElementById("square23").innerHTML){
+               console.log("in");
+               count++;
+            }
+            else if(calledNums[i] == document.getElementById("square4").innerHTML){
+               console.log("in");
+               count++;
+            }
+         }
+         console.log(count);
+         //
+      }
+      
+      if(count == 4){
+         $("#fourCornerWin").show();
+         
+         console.log("You have 4 coners");
+      }
+      else{
+         $("#notFinished4Corners").show();
+            //console.log(document.getElementById("square0"));
+         //console.log("NAH DAWG");
+            //var value = document.getElementById("square0").val();
+            //console.log(value);
+      } 
+       
+   })
+
+   $("#seeLeaderboard").click(function(){
+      $("#container").show();
+      $("#Login").hide();
+      $("#register").hide();
+      $("#seeLeaderboard").hide();
+   })
+
 }
 
 function getRandNumDisplayed(){
    //trying to make the random number generator work from teh server side once the play button is clicked
    $("#play").click(function(){
       $("#numberGenerator").show();
+      $("#play").hide();
       var bingo = {
          selectedNumbers: [],
          generateRandom: function() {
@@ -141,6 +212,7 @@ function getRandNumDisplayed(){
             return random;
          }
       };
+      
       $('.numbersTable td').each(function() {
          var concatClass = this.cellIndex + "" + this.parentNode.rowIndex;
          var numberString = parseInt(concatClass, 10).toString();
@@ -149,83 +221,67 @@ function getRandNumDisplayed(){
       
       
       $('#btnGenerate').click(function() {
-         setInterval(function(){
-            var random = bingo.generateNextRandom().toString();
-            console.log(random);
-            calledNums.push(random);
-            $('.bigNumberDisplay span').text(random);
-            $('.numbersTable td.cell' + random).addClass('selected');
-         }, 3000);  
+      //while(bingo.selectedNumbers.length <= 79){   
+         var random = bingo.generateNextRandom().toString();
+         //console.log(random);
+         calledNums.push(random);
+         $('.bigNumberDisplay span').text(random);
+         $('.numbersTable td.cell' + random).addClass('selected');
+         //sleep(1000);
+      //}
       });
    });
 }
-
-function hash(password){
-   var ret = 0;
-   for(i in password){
-      ret = Math.pow(password[i]*31, password.length-i-1);
-   }
-   return ret;
-}
-
-function makeCard(i){
-	console.log("creating card " + i)
-	var c = "<p>Card number "+i+"</p><table><thead><tr><th width=\"20%\">B</th><th width=\"20%\">I</th><th width=\"20%\">N</th><th width=\"20%\">G</th><th width=\"20%\">O</th></tr></thead><tbody><tr><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td></tr><tr><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td></tr><tr><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"free\">Free</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td></tr><tr><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td></tr><tr><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td><td id=\"square"+increment(j)+"\">&nbsp;</td></tr></tbody></table>"
-	return c;
-}
-
-function increment(j){
-	j++;
-	return j;
-}
-
-function newCard() {
-	for (var i=0; i<24; i++) {
-	   setSquare(j);
-	}
-}
-
-function setSquare(thisSquare) {
-	var currSquare = "square" + thisSquare;
-	var colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
-	var colBasis = colPlace[thisSquare] * 15;
-	var newNum;
-
-	do {
-	   newNum = colBasis + getNewNum() + 1;
-	   console.log(newNum);
-	}
-	while (usedNums[newNum]);
-
-	usedNums[newNum] = true;
-	document.getElementById(currSquare). innerHTML = newNum;
-}
-
-function getNewNum() {
-	return Math.floor(Math.random() * 15);
-}
-function anotherCard() {
-	for (var i=1; i<usedNums.length; i++) {
-	   usedNums[i] = false;
-	}
-
-	newCard();
-	return false;
-}
-
 function sleep(delay) {
     var start = new Date().getTime();
     while (new Date().getTime() < start + delay);
 }
-
 function clickHandler(){
    $(document).on("click","#mytable tbody td", function(e){
       $(this).css("background-color", color);
       console.log(e.target.innerHTML);
    });
 }
+function makeCard(i){
+   var c = "<table><thead><tr><th width=\"20%\">B</th><th width=\"20%\">I</th><th width=\"20%\">N</th><th width=\"20%\">G</th><th width=\"20%\">O</th></tr></thead><tbody><tr><td id=\"square0\">&nbsp;</td><td id=\"square1\">&nbsp;</td><td id=\"square2\">&nbsp;</td><td id=\"square3\">&nbsp;</td><td id=\"square4\">&nbsp;</td></tr><tr><td id=\"square5\">&nbsp;</td><td id=\"square6\">&nbsp;</td><td id=\"square7\">&nbsp;</td><td id=\"square8\">&nbsp;</td><td id=\"square9\">&nbsp;</td></tr><tr><td id=\"square10\">&nbsp;</td><td id=\"square11\">&nbsp;</td><td id=\"free\">Free</td><td id=\"square12\">&nbsp;</td><td id=\"square13\">&nbsp;</td></tr><tr><td id=\"square14\">&nbsp;</td><td id=\"square15\">&nbsp;</td><td id=\"square16\">&nbsp;</td><td id=\"square17\">&nbsp;</td><td id=\"square18\">&nbsp;</td></tr><tr><td id=\"square19\">&nbsp;</td><td id=\"square20\">&nbsp;</td><td id=\"square21\">&nbsp;</td><td id=\"square22\">&nbsp;</td><td id=\"square23\">&nbsp;</td></tr></tbody></table>"
+   $("#mytable").append(c);
+}
+//fix this
+function newCard(whichCard) {
+     for (var i=0; i<24; i++) {
+        setSquare(i);
+     }
+}
+//fix this
+function setSquare(thisSquare, whichCard) {
+     var currSquare = "square" + thisSquare;
+     var colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
+     var colBasis = colPlace[thisSquare] * 15;
+     var newNum;
 
+     do {
+        newNum = colBasis + getNewNum() + 1;
+        //console.log(newNum);
+     }
+     while (usedNums[newNum]);
 
+     usedNums[newNum] = true;
+     document.getElementById(currSquare). innerHTML = newNum;
+}
+function getNewNum() {
+     return Math.floor(Math.random() * 15);
+}
+function anotherCard() {
+     for (var i=1; i<usedNums.length; i++) {
+        usedNums[i] = false;
+     }
 
+     newCard();
+     return false;
+}
 $(initAll);
+
+//new user gets 2000 money
+//every card costs 100 money
+//all of the money goes into the pool
 
